ae75704f7d5727ad3a640fac66c39335f4f58b86
Sergei Aslanov <courageous@bk.ru>
2023-09-14 16:16:07 +0300

    Выполнил задание 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name  # Приватный атрибут name
         self.price = price
         self.quantity = quantity
         Item.all_items.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self._name = value
+        else:
+            self._name = value[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -49,3 +62,28 @@ class Item:
         :return: Список всех товаров.
         """
         return cls.all_items
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str) -> None:
+        """
+        Создает экземпляры класса Item из данных CSV-файла.
+
+        :param file_path: Путь к CSV-файлу с данными.
+        """
+        with open(file_path, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(value: str) -> float:
+        """
+        Преобразует строку в число.
+
+        :param value: Строка, представляющая число.
+        :return: Преобразованное число.
+        """
+        return float(value)
--- tests/test_item.py
+++ tests/test_item.py
@@ -43,3 +43,24 @@ def test_get_all_items(create_items):
     assert len(all_items) == 2
     assert item1 in all_items
     assert item2 in all_items
+
+def test_name_setter():
+    # Проверяем, что сеттер для имени товара обрезает его до 10 символов
+    item = Item('TestItem', 10.0, 5)
+    item.name = 'VeryLongItemName'
+    assert item.name == 'VeryLongIte'  # Ожидаем обрезание до 10 символов
+
+def test_instantiate_from_csv():
+    # Предположим, у вас есть CSV-файл 'test_items.csv' с товарами
+    Item.instantiate_from_csv('test_items.csv')
+    all_items = Item.get_all_items()
+    assert len(all_items) == 3  # Проверяем, что экземпляры были созданы из CSV-файла
+
+def test_string_to_number():
+    # Проверяем, что строковые числа корректно преобразуются в числа с плавающей запятой
+    assert Item.string_to_number('5') == 5.0
+    assert Item.string_to_number('5.0') == 5.0
+    assert Item.string_to_number('5.5') == 5.5
+
+if __name__ == '__main__':
+    pytest.main()
